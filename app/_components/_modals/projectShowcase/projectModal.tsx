2a96059de6f16d221438651c0e41d82462739ba6
Ashade Samson <ashadesamson99@gmail.com>
2025-06-10 08:48:22 +0100

    [feat]: Add videoplay feature for projects in the portfolio section

---
+++ app/_components/_modals/projectShowcase/projectModal.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React from "react";
+import styles from "./projectModal.module.css";
+
+interface ProjectModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  videoSrc: string;
+  title: string;
+}
+
+const ProjectModal: React.FC<ProjectModalProps> = ({ isOpen, onClose, videoSrc, title }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.modal}>
+        <button onClick={onClose} className={styles.closeBtn}>&times;</button>
+        <h3 className={styles.modalTitle}>{title}</h3>
+        <video className={styles.video} src={videoSrc} autoPlay controls />
+      </div>
+    </div>
+  );
+};
+
+export default ProjectModal;
--- app/_components/_sections/(landingPage)/portfolio/portfolio.tsx
+++ app/_components/_sections/(landingPage)/portfolio/portfolio.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
 import styles from './portfolio.module.css';
 import { projects } from '@/app/_utils/fake_data/endpoint';
+import Link from 'next/link';
+import ProjectModal from '@/app/_components/_modals/projectShowcase/projectModal';
 
 const Portfolio = () => {
+
+  const [activeProject, setActiveProject] = useState<null | typeof projects[0]>(null);
+
+  const openModal = (project: typeof projects[0]) => setActiveProject(project);
+  const closeModal = () => setActiveProject(null);
 
 
   return (
@@ -26,11 +35,23 @@ const Portfolio = () => {
                 <p className={styles.projectDescription}>{project.description}</p>
               </div>
               <div className={`${styles.hoverOverlay} group-hover:opacity-100`}>
-                <a href="#" className={`${styles.ctaButton} hover:bg-opacity-90`}>View Case Study</a>
+                <Link onClick={() => openModal(project)}
+                   className={`${styles.ctaButton} hover:bg-opacity-90`} href="#portfolio">
+                    View Case Study
+                </Link>
               </div>
             </div>
           ))}
         </div>
+
+        {activeProject && (
+          <ProjectModal
+            isOpen={!!activeProject}
+            onClose={closeModal}
+            videoSrc={activeProject.video}
+            title={activeProject.title}
+          />
+        )}
 
         {/* <div className={styles.viewAllWrapper}>
           <a href="#" className={styles.viewAllButton}>
