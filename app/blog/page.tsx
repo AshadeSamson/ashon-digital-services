0ec7f99dba607c42491509628257ab7968d6fe09
Ashade Samson <ashadesamson99@gmail.com>
2025-07-01 09:25:38 +0100

    [style]: Update stylling for blog page to match brand style

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import styles from './blog.module.css';
-import Link from 'next/link';
-import Image from 'next/image';
 
 
 export const metadata = {
-  title: 'Our Blog | Coming Soon',
-  description: 'Explore helpful articles, tutorials, and insights on web development and digital growth. Our blog is coming soon!',
+  title: 'Blog | Web Design, Development & Digital Growth Insights – Ashon Digital Services',
+  description: 'Discover expert tips, tutorials, and case studies on web design, development, and digital strategy. Stay informed with the latest insights from Ashon Digital Services.',
 };
 
 
@@ -17,22 +15,14 @@ const Blog = () => {
     <>
       <section className={styles.main}>
         <div className={styles.mainContent}>
-          <div className={`${styles.floating} ${styles.iconContainer}`}>
-            <Image src="/images/favicon.png" alt="Blog Icon" width={45} height={45} />
-          </div>
-
-          <h1 className={styles.headline}>
-            <span className={styles.gradientText}>Our Blog</span><br /> is Coming Soon
-          </h1>
-
-          <p className={styles.subtext}>
-            We’re working behind the scenes to launch a resourceful blog packed with insights on web development, design trends, and business growth. Stay tuned!
-          </p>
-
-          <div className={styles.buttonGroup}>
-            <Link href="https://wa.link/y6dil4" target="_blank" rel="noopener noreferrer">
-              <button className={styles.contactButton}>Contact Us</button>
-            </Link>
+          <div className={styles.textSplit}>
+            <h1 className={styles.headline}>
+              <span className={styles.gradientText}>Our Blog</span><br /> is Coming Soon
+            </h1>
+            <div className={styles.divider}></div>
+            <p className={styles.subtext}>
+              We’re working behind the scenes to launch a resourceful blog packed with insights on web development, design trends, and business growth. Stay tuned!
+            </p>
           </div>
         </div>
       </section>
