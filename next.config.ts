9848c546a0766c3fa13c6ab337997ee4e4985dbb
Ashade Samson <ashadesamson99@gmail.com>
2025-07-27 11:17:22 +0100

    [fix]: Remove the embedded sanity studio path from the app

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
 };
 
-export default nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
